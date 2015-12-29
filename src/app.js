9c41c7c4adf2d2cbe33b1216c3b4865e8ca7e44d
Ryan Allred <Ryan@Allred.xyz>
2015-12-28 21:33:16 -0700

    New Layout

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.riot = require('riot');
 window.$ = require('jquery');
 
@@ -5,4 +7,67 @@ window.$ = require('jquery');
 window.App = riot.observable()
 
 
-//Plugins
+// Pouch database
+var PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-find'));
+window.db = new PouchDB('OctaveDB');
+
+
+// Import
+// Artist: "O'Donnell/Salvatori TotalAudio"
+// Bit Rate: 128
+// Comments: " 00000236 00000000 00000754 00000000 00015FD5 00000000 00005ED0 00000000 00015FA7 00000000"
+// Date Added: "2015-11-15T03:02:33Z"
+// Date Modified: "2015-11-15T04:11:31Z"
+// File Folder Count: 5
+// Genre: "Soundtrack"
+// Kind: "MPEG audio file"
+// Library Folder Count: 1
+// Location: "file:///Users/ryan/Music/iTunes/iTunes%20Media/Music/O'Donnell_Salvatori%20TotalAudio/Unknown%20Album/_Halo_.mp3"
+// Name: ""Halo""
+// Persistent ID: "7ADB397101A2FC4A"
+// Sample Rate: 44100
+// Size: 2912881
+// Skip Count: 1
+// Skip Date: "2015-12-11T07:35:28Z"
+// Total Time: 181995
+// Track ID: 9667
+// Track Type: "File"
+// Year: 1999
+(function(){
+  var os = require('os')
+  var itunesDB = require("itunes-db").loadSync("/Users/ryan/Music/iTunes/iTunes Music Library.xml")
+  var tracks = itunesDB.tracks
+
+  db.allDocs({
+    include_docs: true,
+    attachments: false
+  }).then(function (result) {
+    App.initDocs = result;
+
+    for(var i in tracks)
+      if (tracks[i].Location)
+        add(result,tracks[i])
+
+  }).catch(function (err) {
+    console.log(err);
+  });
+
+  function add(docs,track) {
+    for (let i in docs.rows)
+      if (docs.rows[i].doc.location === track.Location)
+        return; // Found and exit.
+
+    db.post({
+      name: track.Name,
+      album: track.Album,
+      artist: track.Artist,
+      location: track.Location
+    }).then(function(added){
+      console.log('Added',added)
+    }).catch(function(e){
+      console.log(e)
+    })
+  }
+
+})//();
--- tags/tags.js
+++ tags/tags.js
@@ -2,24 +2,43 @@ riot.tag2('anim', '<yield></yield>', 'anim[in]{opacity:1;transform:scale(1) tran
 this.mixin(Power.anim);
 });
 
-riot.tag2('app', '<panel width="100vw" height="80px" class="toolbar-panel"></panel> <panel width="20vw" bottom="0" top="80px" class="menu-panel"> </panel> <panel width="80vw" bottom="0" top="80px" right="0" class="main-panel"> <cell pos="T0 L0 R0 H8" style="background: blue;"> </cell> <cell pos="T8 L0 R0 B0" class="songs"> <track each="{val, i in parent.parent.tracks}" index="{i}" data="{val}"></track> </cell> </panel>', 'html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:\'\';content:none}table{border-collapse:collapse;border-spacing:0}html{background:#202020}app{font-family:sans-serif;color:#eee;display:block}app table{table-layout:fixed}app .toolbar-panel{background:linear-gradient(#ccc, #aaa)}app .menu-panel{background:#202020}app .main-panel{background:#efefef}app .main-panel .songs{overflow:auto}', '', function(opts) {
-var self = this;
-var os = require('os');
-var itunesDB = require("itunes-db").loadSync("/Users/ryan/Music/iTunes/iTunes Music Library.xml");
-
-self.tracks = itunesDB.tracks;
-
-self.tracks.sort(function (a, b) {
-  var keyA = a.Name.trim(),
-      keyB = b.Name.trim();
-  if (keyA < keyB) return -1;
-  if (keyA > keyB) return 1;
-  return 0;
+riot.tag2('app', '<toolbar> </toolbar> <track-list></track-list>', 'html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:\'\';content:none}table{border-collapse:collapse;border-spacing:0}html{background:#fff;box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}app{display:block;padding-top:110px}', '', function(opts) {
 });
 
-self.tracks.length = 1000; // DEBUG
+riot.tag2('cell', '<yield></yield>', '', '', function(opts) {
+this.mixin(Power.cell);
+});
 
-console.log(self.tracks[0]);
+riot.tag2('crumbs', '', 'crumbs{background:#202020}', '', function(opts) {
+});
+
+riot.tag2('panel', '<yield></yield>', '', '', function(opts) {
+this.mixin(Power.panel);
+});
+
+riot.tag2('toolbar', '<div class="inner"> <div class="left"> <svg viewbox="0 0 24 24"> <path d="M6,18.14V6.14H8V18.14H6M9.5,12.14L18,6.14V18.14L9.5,12.14Z"></path> </svg> <svg viewbox="0 0 24 24" onclick="{playPause}"> <path d="M14,19.14H18V5.14H14M6,19.14H10V5.14H6V19.14Z"></path> </svg> <svg viewbox="0 0 24 24"> <path d="M16,18.14H18V6.14H16M6,18.14L14.5,12.14L6,6.14V18.14Z"></path> </svg> </div> <div class="center"> </div> <div class="right"> <input> </div> </div> <crumbs></crumbs>', 'toolbar{display:block;height:110px;position:fixed;left:0;right:0;top:0;background:white;box-shadow:0 0 2px rgba(0,0,0,0.3),0 0 16px rgba(0,0,0,0.1);overflow:hidden}toolbar .inner{position:absolute;top:60px;height:50px;left:0;right:0;overflow:hidden;z-index:10}toolbar crumbs{position:absolute;left:0;right:0;height:60px;top:0;z-index:9}toolbar .left{padding:4px 0;width:25vw;text-align:center;position:absolute;top:0;left:0;bottom:0;z-index:10;box-shadow:0 0 2px rgba(0,0,0,0.3),0 0 16px rgba(0,0,0,0.1)}toolbar .left svg{width:40px}toolbar .left svg path{fill:#333}toolbar .center{padding:12px 0;text-align:center;position:absolute;top:0;right:25vw;left:25vw;bottom:0;z-index:9;border-left:1px solid #ccc;border-right:1px solid #ccc;background:rgba(0,0,0,0.03)}toolbar .right{width:25vw;text-align:center;position:absolute;top:0;right:0;bottom:0;z-index:10;box-shadow:0 0 2px rgba(0,0,0,0.3),0 0 16px rgba(0,0,0,0.1)}toolbar .right input{border:1px solid #ccc;border-radius:3px;position:absolute;top:10px;right:10px;height:30px;width:calc(90%);padding:0 8px}toolbar .right input:focus{outline:none}', '', function(opts) {
+var self = this;
+self.playPause = function () {
+  App.trigger('play_pause');
+};
+}, '{ }');
+
+riot.tag2('track-list', '<track each="{val, i in tracks}" index="{i}" data="{val}"></track>', 'track-list{font-family:sans-serif;color:#333;display:block;width:100%;white-space:nowrap}track-list .cell{padding:8px;display:inline-block;float:left}track-list .cell:nth-child(1){width:45vw}track-list .cell:nth-child(2){width:30vw}', '', function(opts) {
+var self = this;
+self.tracks = [];
+
+db.allDocs({
+  include_docs: true,
+  attachments: false,
+  limit: 10000
+}).then(function (result) {
+
+  for (var i in result.rows) self.tracks.push(result.rows[i].doc);
+
+  self.update();
+}).catch(function (err) {
+  console.log(err);
+});
 
 var track = null;
 var interval = null;
@@ -30,7 +49,7 @@ App.on('select_track', function (index) {
 
   if (track) track.pause();
 
-  track = new Audio(self.tracks[index].Location);
+  track = new Audio(self.tracks[index].location);
   track.play();
 
   self.updateTime();
@@ -39,7 +58,7 @@ App.on('select_track', function (index) {
 self.updateTime = () => {
   if (track.paused) return;
 
-  App.trigger('track_time_update', track.currentTime);
+  App.trigger('track_time_update', track.currentTime, track.duration);
   interval = window.setTimeout(self.updateTime, 1000);
 };
 
@@ -55,21 +74,12 @@ App.on('play_pause', () => {
 });
 }, '{ }');
 
-riot.tag2('cell', '<yield></yield>', '', '', function(opts) {
-this.mixin(Power.cell);
-});
-
-riot.tag2('panel', '<yield></yield>', '', '', function(opts) {
-this.mixin(Power.panel);
-});
-
-riot.tag2('track', '<div class="data center">{data.Name}</div> <div class="data right"> <span if="{active && currentTimeStr}">{currentTimeStr} / </span> {timeStr} </div> <div class="progress" if="{active}"> <div class="value"> </div> </div>', 'track{cursor:pointer;display:block;position:relative;overflow:hidden;color:#444;height:40px}track:nth-child(even){background:rgba(0,0,0,0.01)}track:hover{color:black;background:rgba(0,0,0,0.1)}track.active{color:#444;background:#eee}track .data{display:inline-block;margin:0 20px;line-height:40px}track .data.left{width:30px;text-align:right}track .data.center{min-width:400px;width:80vw}track .data.right{position:absolute;right:0;top:0}track h2{color:white;background:black;border-radius:3px;position:absolute;top:20px;left:100px;line-height:40px;font-size:25px;display:inline-block;padding:0 32px}track h3{color:white;background:black;border-radius:3px;position:absolute;top:62px;left:100px;line-height:30px;font-size:15px;display:inline-block;padding:0 32px}track .circle{width:70px;height:70px;background:#ff8000;position:absolute;top:20px;left:20px;border:none;border-radius:1000px}track .circle:active,track .circle.playing{background:#0080ff}track .circle.playing:active{background:#ff8000}track .progress{position:absolute;left:20px;bottom:0;right:20px;height:6px;background:#101010;border-radius:1000px;overflow:hidden}track .progress .value{background:#ff8000;border-radius:1000px;position:absolute;top:1px;left:1px;bottom:1px;width:100%;transform-origin:0;tranform:translateX(-100%)}', 'onclick="{select}"', function(opts) {
+riot.tag2('track', '<div class="cell">{data.name}</div> <div class="cell">{data.album}</div> <div class="cell">{data.artist}</div>', 'track{cursor:pointer;overflow:hidden;color:#000;display:block;width:100%}track:nth-child(even){background:rgba(0,0,0,0.01)}track:hover{color:black;background:rgba(0,0,0,0.1)}track.active{color:#eee;background:#0080ff}track .cell{text-overflow:ellipsis;overflow:hidden}', 'onclick="{select}"', function(opts) {
 var self = this;
 self.data = self.opts.data;
 self.index = self.opts.index;
-self.time = new Date(self.data['Total Time']);
-
-self.timeStr = self.time.getMinutes() + ':' + (self.time.getSeconds() < 10 ? '0' : '') + self.time.getSeconds();
+// self.time = new Date(self.data['Total Time'])
+// self.timeStr = self.time.getMinutes() + ':' + ((self.time.getSeconds()<10)? '0':'') + self.time.getSeconds()
 
 var $r = $(self.root);
 
@@ -94,12 +104,18 @@ self.select = () => {
   self.update();
 };
 
-self.updateTime = time => {
+self.updateTime = (time, duration) => {
   var min = Math.floor(time / 60);
   var sec = Math.floor(time - min * 60);
   self.currentTimeStr = min + ':' + (sec < 10 ? '0' : '') + sec;
+
+  min = Math.floor(duration / 60);
+  sec = Math.floor(duration - min * 60);
+  self.timeStr = min + ':' + (sec < 10 ? '0' : '') + sec;
+
   self.update();
-  var percent = time * 1000 / self.data['Total Time'] * 100;
+
+  var percent = time / duration * 100;
   $r.find('.progress .value').css({ 'transform': 'translateX(-' + (100 - percent) + '%)' });
 };
 
