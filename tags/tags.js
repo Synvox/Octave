e549bb9d2cd4939f36f5642c0878fdcbc2dd4b42
Ryan Allred <Ryan@Allred.xyz>
2015-12-31 12:35:05 -0700

    Space bar bug

--- tags/tags.js
+++ tags/tags.js
@@ -16,6 +16,8 @@ db.allDocs({
 
 $(window).keypress(function (e) {
   if (e.keyCode === 0 || e.keyCode === 32) {
+    if ($('input:focus, textarea:focus').length) return;
+
     e.preventDefault();
     App.trigger('play_pause');
   }
